aeb2227dfc805d0f9737d480b7cc96716810a133
Ramprasanna2 <prasannamoni2006@gmail.com>
2025-08-15 00:39:15 +0530

    final1

--- app/mongodb/connect.tsx
+++ app/mongodb/connect.tsx
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 async function connectDB() {
-  return mongoose.connect(MONGODB_URI);
+  return mongoose.connect("mongodb+srv://RAMPRASANNA:rockyy@project.26nbhxz.mongodb.net/?retryWrites=true&w=majority&appName=project");
 }
 
 
